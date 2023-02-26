1c86ed5fd0d6c27170957d118e333e64e7de1177
Jaremy Creechley <creechley@gmail.com>
2023-02-26 02:43:14 -0700

    nim paths

--- src/ants.nim
+++ src/ants.nim
@@ -1,7 +1,7 @@
 import sequtils, macros, options
 import pegs, tables
 import nimscripter, nimscripter/variables
-import json
+import json, osproc
 
 import msgpack4nim
 import msgpack4nim/msgpack2json
@@ -20,18 +20,13 @@ const dflOpts = AntsOptions(
   bin: false
 )
 
-var systems = @[
-  "/Users/jaremycreechley/.asdf/installs/nim/1.6.10/nimble/pkgs/flatty-0.3.4",
-  "/Users/jaremycreechley/.asdf/installs/nim/1.6.10/nimble/pkgs/msgpack4nim-0.3.1",
-]
-
 proc fromVm*(t: typedesc[Peg], node: PNode): Peg =
   if node.kind == nkStrLit:
     peg(node.strVal)
   else:
     raise newException(VMParseError, "Cannot convert to: " & $t)
 
-proc runConfigScript*[T](typ: typedesc[T], path: string): T =
+proc runConfigScript*[T](typ: typedesc[T], path: string, systems: seq[string]): T =
   let
     intr = loadScript(
       NimScriptPath(path),
@@ -45,7 +40,7 @@ proc runConfigScript*[T](typ: typedesc[T], path: string): T =
   #   antConfigValue: T
   # result = antConfigValue
   
-proc runConfigScript*(path: string): string =
+proc runConfigScript*(path: string, systems: seq[string]): string =
   let
     intr = loadScript(
       NimScriptPath(path),
@@ -66,7 +61,15 @@ when isMainModule: # Preserve ability to `import api`/call from Nim
   import cligen
 
   var app = initFromCL(dflOpts, short = Short)
-  let res = runConfigScript(app.file)
+
+  let nimDump = execProcess("nim dump --verbosity:0 --dump.format:json json").
+                    parseJson()["lib_paths"]
+  var systems: seq[string]
+  for pthjn in nimDump:
+    let pth = pthjn.getStr()
+    if pth.contains("ants"): systems.add(pth)
+
+  let res = runConfigScript(app.file, systems)
   if app.bin:
     echo res
   elif app.hex:
